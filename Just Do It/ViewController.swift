ebf3968c2091560a3171291756036bb25781bb08
iamsimranjot <iamsimranjot@gmail.com>
2016-12-07 22:18:26 +0530

    Initial Commit.

---
+++ Just Do It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Just Do It
+//
+//  Created by SimranJot Singh on 07/12/16.
+//  Copyright © 2016 SimranJot Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
